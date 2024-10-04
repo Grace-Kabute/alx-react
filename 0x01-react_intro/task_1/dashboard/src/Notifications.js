801a93aee326c26ab60d9e803741d5e615851929
babyGirl <kabutemuthoni@gmail.com>
2024-10-04 17:11:53 +0300

    React Intro

---
+++ 0x01-react_intro/task_1/dashboard/src/Notifications.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './Notifications.css'
+
+function Notifications(){
+<div className="notifications">
+    <p>Here is the list of notifications</p>
+</div>
+}
+
+export default Notifications
